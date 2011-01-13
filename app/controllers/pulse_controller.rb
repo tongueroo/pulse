c938c9010116bbf8547e97e6a689ed2a893313d8
Tung Nguyen <tongueroo@gmail.com>
2011-01-12 21:03:03 -0800

    pulse controller

---
+++ app/controllers/pulse_controller.rb
@@ -0,0 +1,5 @@
+class PulseController < ApplicationController
+  def index
+    render :text => "OK"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Takebackyourpower::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match '' => 'pulse#index'
+  match 'pulse' => 'pulse#index'
+  
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
