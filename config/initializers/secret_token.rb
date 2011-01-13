8cc6c77541619689adfc3e6ecf257dfb08b48069
Tung Nguyen <tongueroo@gmail.com>
2011-01-12 20:56:14 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Takebackyourpower::Application.config.secret_token = '8164be28df6ea1cb1048b722bb80db1a3e5a527b18196cbeda0b8ae6d231c951a757d9b60e8d8a597ff9e6ee231f401a1d601b71495e377fa5b0b80a0737bfca'
